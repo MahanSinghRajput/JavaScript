a280a6082d0a72222538e9e0b35761c6d67ddb6a
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-31 15:22:13 +0530

    datatypes added

---
+++ basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+//by default it is on, no need to write this, but it is a good practice.
+
+// alert() // this works in browser not in nodeJS
+
+//number (2^53), bigint, string('' or ""), boolean, null (standalone value)
+//undefined, symbol (unique)
+//object 
+
+let a = ""
+console.log(typeof a)
+console.log(typeof undefined) //undefined
+console.log(typeof null) // it gives "object" as output which maybe called as an error of the language
