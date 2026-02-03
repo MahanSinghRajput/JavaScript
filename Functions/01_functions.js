f8ef872a8da28ad17aeca9ed602f5353be56ed28
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-03 18:39:28 +0530

    added more about functions

--- Functions/01_functions.js
+++ Functions/01_functions.js
@@ -24,3 +24,39 @@ console.log(typeof abc(4))
         //code
     }
 */
+
+
+//for multiple arguments 
+//rest operator bundles the arguments
+function calculateCartPrice(...nums){ //rest operator
+    return nums
+}
+console.log(calculateCartPrice(200,300,300,400)) //gives an array
+//[ 200, 300, 300, 400 ]
+
+function calculateCartPrice2(val1,val2,...nums){ //rest operator
+    return nums
+}
+console.log(calculateCartPrice2(200,300,300,400))
+//[ 300, 400 ]
+
+const user = {
+    username: "abc",
+    price: 999
+}
+
+function handleObject(anyObject){
+    console.log(`This is ${anyObject.username} and his cart price is ${anyObject.price}`)
+}
+handleObject(user) //if user has "prices" instead of "price" then it will show undefined
+handleObject({
+    username: "abc",
+    price:100
+})
+
+
+const newArr = [10,20,30,40]
+function secondValue(arr){
+    return arr[1]
+}
+console.log(secondValue(newArr))
