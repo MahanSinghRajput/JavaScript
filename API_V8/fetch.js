73fbdc5dd920be0be5c66f36a65ba3073b0e614b
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-03-24 19:26:19 +0530

    fetch

---
+++ API_V8/fetch.js
@@ -0,0 +1,3 @@
+//fetch is a global method for fetching a resource from the network.
+//fetch uses a special queue to reach the call stack, that's why it's response is faster.
+//see fetchStructure.png for internal structure
